fdf197c70e46bccfae7dd6600f6e3c34233e723a
aleksiDu <aleksi.duluzauri@gmail.com>
2023-09-12 18:41:46 +0400

    Fix commit

--- index.tsx
+++ index.tsx
@@ -15,23 +15,29 @@ console.log(`Listening on http://${server.hostname}:${server.port}`);
 async function handler(request: Request): Promise<Response> {
   const url = new URL(request.url);
 
+  if (url.pathname === "/todos" && request.method === "POST") {
+    try {
+      const { todo } = await request.json();
+
+      if (!todo?.length) return new Response("Invalid input", { status: 500 });
+
+      todos.push({
+        id: todos.length + 1,
+        text: todo,
+      });
+
+      return new Response(renderToString(<TodoList todos={todos} />));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   if (url.pathname === "" || url.pathname === "/") {
     return new Response(Bun.file("index.html"));
   }
-  if (url.pathname === "/todos" && request.method === "POST") {
-    const { todo } = await request.json();
 
-    if (todo?.length) return new Response("Invalid input", { status: 500 });
-
-    todos.push({
-      id: todos.length + 1,
-      text: todo,
-    });
-
-    return new Response(renderToString(<TodoList todos={todos} />));
-  }
   if (url.pathname === "/todos" && request.method === "GET") {
-    return new Response(renderToString(<TodoList todos={[]} />));
+    return new Response(renderToString(<TodoList todos={todos} />));
   }
   return new Response("Not Found", { status: 404 });
 }
